bc1d98f14fb1cfbff7cc646bfd25ee76f8cb2e72
mmpsj <127182538+mmpsj@users.noreply.github.com>
2024-04-03 22:54:12 +0900

    [Silver III] Title: queuestack, Time: 596 ms, Memory: 62644 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.java
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -6,28 +6,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] qs = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        Deque<Integer> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            qs[i] = Integer.parseInt(st.nextToken());
-        }
-        int[] c = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            c[i] = Integer.parseInt(st.nextToken());
-        }
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = n - 1; i >= 0; i--) {
-            if (qs[i] == 0) {
-                q.add(c[i]);
+            if (Integer.parseInt(st1.nextToken()) == 0) {
+                q.addFirst(Integer.parseInt(st2.nextToken()));
+            } else {
+                st2.nextToken();
             }
         }
         int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        st1 = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            q.add(num);
-            sb.append(q.remove()).append(" ");
+            int num = Integer.parseInt(st1.nextToken());
+            q.addLast(num);
+            sb.append(q.removeFirst()).append(" ");
         }
         System.out.print(sb.toString());
     }
