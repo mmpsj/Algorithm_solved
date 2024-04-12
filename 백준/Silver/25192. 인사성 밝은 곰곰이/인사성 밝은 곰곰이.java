b1ce76dcd427b51f0d9bb4bc166db3f6dbc6e00b
mmpsj <127182538+mmpsj@users.noreply.github.com>
2024-04-12 20:34:38 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 276 ms, Memory: 25444 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -4,20 +4,19 @@ import java.util.*;
 public class Main {
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            Map<String, Character> member = new HashMap<>();
+            Set<String> member = new HashSet<>();
             int n = Integer.parseInt(br.readLine());
             int count = 0;
             for (int i = 0; i < n; i++) {
                 String temp = br.readLine();
-                if (temp.compareTo("ENTER") == 0) {
+                if (temp.equals("ENTER")) {
+                    count += member.size();
                     member.clear();
                 } else {
-                    if (member.get(temp) == null) {
-                        member.put(temp, ' ');
-                        count++;
-                    }
+                    member.add(temp);
                 }
             }
+            count += member.size();
             System.out.print(count);
         }
 }
