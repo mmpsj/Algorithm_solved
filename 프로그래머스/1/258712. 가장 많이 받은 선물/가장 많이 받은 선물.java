3f462c3cb036b635196064c07d1215bb378930fe
mmpsj <127182538+mmpsj@users.noreply.github.com>
2024-04-12 15:31:46 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 3.52 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
         int len = friends.length;
+        int[] gift_amount = new int[len];
         int[][] log = new int[len][len];
         Map<String, Integer> friend_idx = new HashMap<>();
         for (int i = 0; i < len; i++) {
@@ -10,20 +11,24 @@ class Solution {
         StringTokenizer st;
         for (int i = 0; i < gifts.length; i++) {
             st = new StringTokenizer(gifts[i]);
-            log[friend_idx.get(st.nextToken())][friend_idx.get(st.nextToken())] += 1;
+            String fr1 = st.nextToken();
+            String fr2 = st.nextToken();
+            gift_amount[friend_idx.get(fr1)]++;
+            gift_amount[friend_idx.get(fr2)]--;
+            log[friend_idx.get(fr1)][friend_idx.get(fr2)]++;
         }
         int[] will = new int[len];
         for (int i = 0; i < len - 1; i++) {
             for (int j = i + 1; j < len; j++) {
                 if (log[i][j] > log[j][i]) {
-                    will[i] += 1;
+                    will[i]++;
                 } else if (log[i][j] < log[j][i]) {
-                    will[j] += 1;
+                    will[j]++;
                 } else {
-                    if (gift_amount(log, i, len) > gift_amount(log, j, len)) {
-                        will[i] += 1;
-                    } else if (gift_amount(log, i, len) < gift_amount(log, j, len)) {
-                        will[j] += 1;
+                    if (gift_amount[i] > gift_amount[j]) {
+                        will[i]++;
+                    } else if (gift_amount[i] < gift_amount[j]) {
+                        will[j]++;
                     }
                 }
             }
@@ -33,14 +38,5 @@ class Solution {
             if (will[i] > max) max = will[i];
         }
         return max;
-    }
-    
-    public int gift_amount(int[][] log, int i, int len) {
-        int sum = 0;
-        for (int j = 0; j < len; j++) {
-            sum += log[i][j];
-            sum -= log[j][i];
-        }
-        return sum;
     }
 }
