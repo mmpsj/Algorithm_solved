f01370fffe9f84f52d9bb683f91df412c4f5f621
mmpsj <127182538+mmpsj@users.noreply.github.com>
2024-04-16 23:37:42 +0900

    [Silver III] Title: N과 M (3), Time: 460 ms, Memory: 68844 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static StringBuilder sb = new StringBuilder();
+    public static int n, m;
+    public static int[] visit;
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visit = new int[n];
+        arr = new int[m];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (visit[i] < m) {
+                visit[i]++;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visit[i]--;
+            }
+        }
+    }
+}
