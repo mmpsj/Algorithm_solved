42b674ef1e3bad6bfb5721dd750a4b4978f57ae8
mmpsj <127182538+mmpsj@users.noreply.github.com>
2024-04-16 21:57:03 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 536 ms, Memory: 46784 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        sb.append((int)Math.pow(2, n) - 1).append("\n");
+        hanoi(sb, n, 1, 2, 3);
+        System.out.print(sb);
+    }
+
+    public static void hanoi(StringBuilder sb, int n, int from, int temp, int to) {
+        if (n == 1) {
+            sb.append(from).append(' ').append(to).append('\n');
+            return;
+        }
+        hanoi(sb, n - 1, from, to, temp);
+        sb.append(from).append(' ').append(to).append('\n');
+        hanoi(sb, n - 1, temp, from, to);
+    }
+}
